0bf14654f2892198e976f4c2b91d49d1e27e326e
Roman Doronin <roma666132@yandex.ru>
2020-03-05 21:21:33 +0300

    Refactor by folder

--- SimpleHTTPServer/Program.cs
+++ SimpleHTTPServer/Program.cs
@@ -1,49 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using SimpleHTTPServer.HTTPInteraction;
 
 namespace SimpleHTTPServer
 {
-    class Server
+    class Program
     {
-        private TcpListener listener;
-
-        static void ClientThread(Object stateInfo)
-        {
-            new Client((TcpClient)stateInfo);
-        }
-
-        public Server(int port)
-        {
-            listener = new TcpListener(IPAddress.Any, port);
-            listener.Start();
-
-            while (true)
-            {
-                // Принимаем нового клиента
-                TcpClient client = listener.AcceptTcpClient();
-
-                // Создаем поток
-                Thread thread = new Thread(new ParameterizedThreadStart(ClientThread));
-
-                // И запускаем этот поток, передавая ему принятого клиента
-                thread.Start(client);
-            }
-        }
-
-        ~Server()
-        {
-            if (listener != null)
-            {
-                listener.Stop();
-            }
-        }
-
         static void Main(string[] args)
         {
             new Server(80);
--- doc/DataBase.md
+++ doc/DataBase.md
@@ -1,18 +1,19 @@
-База данных
+# Database schema
+
 >User (Врач)
->>account_id
->>firstname
->>secondname
->>username
->>hash_password
+>>* account_id
+>>* firstname
+>>* secondname
+>>* username
+>>* hash_password
 
 >Patient
->>patient_id
->>firstname
->>secondname
->>pet_medical_card_id
+>>* patient_id
+>>* firstname
+>>* secondname
+>>* pet_medical_card_id
 
 >PetMedicalCard
->>pet_medical_card_id
->>name
->>disease (Название болезни)
+>>* pet_medical_card_id
+>>* name
+>>* disease (Название болезни)
