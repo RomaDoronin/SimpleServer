c72489e663a4ba7d1a41a8f616765e66fa97e2d2
Roman Doronin <roma666132@yandex.ru>
2020-03-05 21:21:59 +0300

    Refactor by folder

---
+++ SimpleHTTPServer/HTTPInteraction/Server.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace SimpleHTTPServer.HTTPInteraction
+{
+    class Server
+    {
+        private TcpListener listener;
+
+        static void ClientThread(object stateInfo)
+        {
+            new Client((TcpClient)stateInfo);
+        }
+
+        public Server(int port)
+        {
+            Console.WriteLine("Server prepare to start");
+            listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+            Console.WriteLine("Server start");
+
+            while (true)
+            {
+                Console.WriteLine("Weit client");
+                // Принимаем нового клиента
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("Client connected");
+
+                // Создаем поток
+                Thread thread = new Thread(new ParameterizedThreadStart(ClientThread));
+
+                // И запускаем этот поток, передавая ему принятого клиента
+                thread.Start(client);
+            }
+        }
+
+        ~Server()
+        {
+            if (listener != null)
+            {
+                listener.Stop();
+            }
+        }
+    }
+}
