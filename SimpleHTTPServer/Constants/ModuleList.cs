a7a97702889eb0b189f144454b5238b12b305499
Roman Doronin <roma666132@yandex.ru>
2020-03-09 17:44:15 +0300

    Add constant folder

---
+++ SimpleHTTPServer/Constants/ModuleList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHTTPServer.Constants
+{
+    class ModuleList
+    {
+        public static Modules.IModule GetModuleByModuleName(string moduleName)
+        {
+            Modules.IModule module;
+
+            switch (moduleName)
+            {
+                case "regist":
+                    module = new Modules.Regist();
+                    break;
+                default:
+                    module = new Modules.Regist();
+                    break;
+            }
+            
+            return module;
+        }
+    }
+}
---
+++ SimpleHTTPServer/Constants/UrlPositionNumber.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHTTPServer.Constants
+{
+    enum UrlPositionNumber
+    {
+        COMPANY_PREFIX = 1,
+        MODULE_NAME = 2
+    }
+}
